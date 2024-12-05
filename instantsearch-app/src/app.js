4f553712c96b684b8c009c7bb605fd4d6511de30
lemonlux <sololucia17@gmail.com>
2024-12-05 12:45:55 +0100

    logo

--- instantsearch-app/src/app.js
+++ instantsearch-app/src/app.js
@@ -21,6 +21,9 @@ search.addWidgets([
   <img src=${ hit.variations[0].image_url } alt=${ hit.name } />
   <div>
     <h1>${components.Highlight({hit, attribute: "name"})}</h1>
+    <div>
+    <img src="https://res.cloudinary.com/daxddugwt/image/upload/v1733398645/eec9a7c7edb19fe3f0cba277d47055c4-removebg-preview_bj4swy.png" alt="Nooktools" /> 
+    </div>
     <p>${components.Highlight({hit, attribute: "sell"})}</p>
     <p>${components.Highlight({hit, attribute: "availability.0.note"})}</p>
   </div>
